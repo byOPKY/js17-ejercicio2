5e27b698e9db3ee379f635c0921940b507cdb914
byOPKY <orrillokarol@gmail.com>
2016-08-25 17:32:51 -0500

    subiendo solo el ejercicio

---
+++ js/javas.js
@@ -0,0 +1,17 @@
+window.alert("Aprendiendo si es Par o Impar");
+var num1 =parseInt(prompt ("Ingresa el numero"));
+
+if (num1%2==0) {
+	document.write("Par");
+}
+else {
+	document.write("Impar");
+}
+
+
+
+
+
+
+
+
